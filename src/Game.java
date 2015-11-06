aeaaae72c12c42f5e4df7d6f72d5f173cc5c0ca2
Evgeniy Yakush <yakush@speroteck.com>
2015-11-06 13:33:58 +0200

    Began implement a game animation

---
+++ src/Game.java
@@ -0,0 +1,133 @@
+import game.gfx.Screen;
+import game.gfx.SpriteSheet;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVarsionUID = 1L;
+
+    public static final int WIDTH = 160;
+    public static final int HEIGHT = WIDTH / 12 * 9;
+    public static final int SCALE = 3;
+    public static final String NAME = "GAME";
+
+    private JFrame frame;
+
+    public boolean running = false;
+    public int tickCount = 0;
+
+    private BufferedImage image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+    //    private SpriteSheet spriteSheet = new SpriteSheet("/paper_mario_sprites_by_havocq.png");
+    private Screen screen;
+
+    public Game() {
+        setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
+        frame = new JFrame(NAME);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(this, BorderLayout.CENTER);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+
+    public void init() {
+        screen = new Screen(WIDTH, HEIGHT, new SpriteSheet("/sprite_sheet.png"));
+    }
+
+    public synchronized void start() {
+        new Thread(this).start();
+        running = true;
+    }
+
+    public synchronized void stop() {
+        running = false;
+    }
+
+    @Override
+    public void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000D / 60D;
+
+        int ticks = 0;
+        int frames = 0;
+
+        long lastTimer = System.currentTimeMillis();
+        double delta = 0;
+
+        init();
+
+        while (running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / nsPerTick;
+            lastTime = now;
+            boolean shouldRender = true;
+            while (delta >= 1) {
+                ticks++;
+                tick();
+                delta -= 1;
+                shouldRender = true;
+            }
+            try {
+                Thread.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (shouldRender) {
+                frames++;
+                render();
+            }
+
+            if (System.currentTimeMillis() - lastTimer >= 1000) {
+                lastTimer += 1000;
+                System.out.println(ticks + " ticks x " + frames + " frames");
+                frames = 0;
+                ticks = 0;
+            }
+        }
+    }
+
+    public void tick() {
+        tickCount++;
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = i + tickCount;
+        }
+    }
+
+    public void render() {
+        BufferStrategy bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+
+        screen.render(pixels, 0, WIDTH);
+        Graphics g = bs.getDrawGraphics();
+
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+        g.setColor(Color.CYAN);
+        g.drawRect(0, 0, getWidth(), getHeight());
+        g.dispose();
+        bs.show();
+    }
+
+    /**
+     * The entry main method
+     */
+    public static void main(String[] args) {
+        new Game().start();
+    }
+
+}
